6fc97a5364dce0d570aaa436c690c9ccf82bb687
Mythili2909 <mythilikumar29092005@gmail.com>
2024-07-23 15:16:17 +0530

    Created a Frontend Application

---
+++ reactapp/src/components/Login.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Username</label>
+                <input
+                    type="email"
+                    placeholder="Enter your Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
+                <label>Password</label>
+                <input
+                    type="password"
+                    placeholder="Enter password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    name="password"
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
